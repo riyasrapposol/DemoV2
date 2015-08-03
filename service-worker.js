c1a6bf7512bd477f3dc555a722810176dac0139d
riyasrapposol <riyasrapposol@users.noreply.github.com>
2015-08-03 22:21:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,69 @@
 'use strict';
 
-self.addEventListener('push', function(event) {
-  console.log('Received a push message', event);
-
-  var title = 'Yay a message.';
-  var body = 'We have received a push message.';
-  var icon = '/images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  );
+self.addEventListener('install', function(e) {
+  console.log('oninstall');
 });
 
+self.addEventListener('activate', function(e) {
+  console.log('onactivate');
+});
 
-self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  // Android doesn’t close the notification when you click on it
-  // See: http://crbug.com/463146
-  event.notification.close();
+self.addEventListener('fetch', function(e) {
+  console.log('onfetch:', e);
+});
 
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url == '/' && 'focus' in client)
-        return client.focus();
+self.addEventListener('push', function(e) {
+  console.log('Push Event Received');
+
+  if (!(self.Notification && self.Notification.permission === 'granted')) {
+    console.error('Failed to display notification - not supported');
+
+    // Perhaps we don't have permission to show a notification
+    if (self.Notification) {
+      console.error('  notificaton permission set to:',
+        self.Notification.permission);
     }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
+    return;
+  }
 
+  var data = {};
+  if (e.data) {
+    data = e.data.json();
+  }
+  var title = data.title || 'No Payload with Message';
+  var message = data.message || 'This will change in future versions of Chrome.';
+  var icon = 'images/touch/chrome-touch-icon-192x192.png';
+
+  var notification = new Notification(title, {
+    body: message,
+    icon: icon,
+    tag: 'simple-push-demo-notification'
+  });
+
+  // This should be swapped out by the notificationclick event
+  notification.addEventListener('click', function() {
+    if (clients.openWindow) {
+      console.log('Notification clicked, trying to call clients.openWindow');
+      clients.openWindow('https://gauntface.com/blog/2014/12/15/push-notifications-service-worker');
+    } else {
+      console.log('Notification clicked, but clients.openWindow is not currently supported');
+    }
+  });
+
+  return notification;
 });
+
+self.addEventListener('pushsubscriptionlost', function(e) {
+  console.log(e);
+});
+
+self.addEventListener('notificationclick', function(e) {
+  console.log('Notification click yo.');
+
+  if (clients.openWindow) {
+    clients.openWindow('https://gauntface.com/blog/2014/12/15/push-notifications-service-worker');
+  } else {
+    console.log('Notification clicked, but clients.openWindow is not currently supported');
+  }
+});
+
